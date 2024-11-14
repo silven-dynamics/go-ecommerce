5b36307f501b77f5ce5ea52960c04f827317d4a3
Steve Wanjohi <14216713+stiffinWanjohi@users.noreply.github.com>
2024-11-15 01:20:25 +0300

    Project Init: Define services, implement grapgQL service

---
+++ graphql/mutation_resolver.go
@@ -0,0 +1,11 @@
+package main
+
+import "context"
+
+type mutationResolver struct {
+	server *Server
+}
+
+func (r *mutationResolver) CreateAccount(ctx context.Context, input AccountInput) (*Account, error)
+func (r *mutationResolver) CreateProduct(ctx context.Context, input ProductInput) (*Product, error)
+func (r *mutationResolver) CreateOrder(ctx context.Context, input OrderInput) (*Order, error)
